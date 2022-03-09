0c620a0148cc746a6eb57e1a117e5b055a8f3743
Lisa Meyer <lisamermey@gmail.com>
2022-03-09 14:10:02 -0500

    working solution

--- middle.js
+++ middle.js
@@ -1,16 +1,26 @@
 /*
-* Function takes a source array and an itemsToRemove array and 
-* returns and array with only those elements from source that are 
-* not present in the itemsToRemove array.
-* @param {Array} source 
-* @param {Array} itemsToRemove
-* @result {Array} arr without matching items in itemsToRemove
+* Function takes an array and returns the middle most element(s) of the given array
+* @param {Array} arr
+* @return {Array} result
 */
-const without = (source, itemsToRemove) => {
-  // filter out itemToRemove and store in a new array
-  const arr = source.filter(item => !itemsToRemove.includes(item));
-  
-  return arr;
+const middle = arr => {
+  const len = arr.length;
+  const midLen = Math.floor(len/2);
+  let result = [];
+
+  // checks if the length of the array is less than or equal to 2
+  // if true returns an empty array
+  if (len <= 2) {
+    return result;
+  } 
+  // checks if array length is odd or even
+  if (!(len % 2 === 0))  {
+      result.push(arr[midLen]);
+  } else {
+      result.push(arr[midLen - 1]);
+      result.push(arr[midLen]);
+  }
+  return result;
 };
 
 /*------------------- TESTS -----------------*/
@@ -43,11 +53,12 @@ const assertArraysEqual = (arr1, arr2, expected) => {
 
 // TEST CASES
 let words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
+middle(words); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"], true);
 
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3], true);
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"], true);
-assertArraysEqual(without([1, 2, 3], [1, 2, 3]), [], true);
-assertArraysEqual(without([1, 2, 3], []), [], false);
+assertArraysEqual(middle([1, 2, 3]), [2], true);
+assertArraysEqual(middle([2, 3]), [], true);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3], true);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4], true);
+assertArraysEqual(middle([1, 2, 3, 4]), [2], false);
