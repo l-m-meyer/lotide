f347256c6590c250b65557d2847668d4790dbf1e
Lisa Meyer <lisamermey@gmail.com>
2022-03-09 15:44:36 -0500

    add assertArraysEqual function and eqArrays function

--- letterPositions.js
+++ letterPositions.js
@@ -8,3 +8,35 @@ const letterPositions = sentence => {
 
   return results;
 };
+
+/*------------------- TESTS -----------------*/
+
+/*
+* Function takes in two arrays and returns true or false based on a perfect match
+* @param {Array} arr1
+* @param {Array} arr2
+* @result {Boolean} true or false
+*/
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } return true;
+};
+
+// TEST FUNCTION FOR ARRAYS
+const assertArraysEqual = (arr1, arr2, expected) => {
+  if (eqArrays(arr1, arr2) === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3], true); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, "3"], false); // => should PASS
+assertArraysEqual([1, 2, 3], [0, 12, -3, 8], false); // => should PASS
