9814c46979de1ba868ac364890006a7295f9ab91
Lisa Meyer <lisamermey@gmail.com>
2022-03-09 16:33:17 -0500

    initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+/*
+*
+*
+*
+*/
+const findKeyByValue = (obj, value) => {
+
+};
+
+
+/*------------------- TESTS -----------------*/
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
