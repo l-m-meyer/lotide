156d07a78308e973bfaf0bfbc4e099427bc8f3c7
Lisa Meyer <lisamermey@gmail.com>
2022-03-09 14:49:31 -0500

    working solution

--- countOnly.js
+++ countOnly.js
@@ -1,15 +1,23 @@
 /*
-*
-*
-*
+* Function will take in an array and an object and return an object 
+* containing the counts of everything that the input object listed.
+* @param {Array} allItems
+* @param {Ojbect} itemsToCount
+* @return {Object} counts
 */
 const countOnly = (allItems, itemsToCount) => {
   const counts = {};
 
   for (let item of allItems) {
-    console.log(item);
+    if (itemsToCount[item]) {
+      if (counts[item]) {
+        counts[item]++;
+      } else {
+        counts[item] = 1;
+      }
+    }
   }
-
+  console.log(counts);
   return counts;
 };
 
