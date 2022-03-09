e945a245d3856a36b3a76fede7ea1980448c7b08
Lisa Meyer <lisamermey@gmail.com>
2022-03-09 15:41:06 -0500

    initial commit

---
+++ letterPositions.js
@@ -0,0 +1,10 @@
+/*
+* Function returns all the indices in a string where each character is found.
+* @param {String}
+* @return {Object}
+*/
+const letterPositions = sentence => {
+  const results = {};
+
+  return results;
+};
