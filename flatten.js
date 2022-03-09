e649c5ca62011af9af6368850c1640b704aeb39e
Lisa Meyer <lisamermey@gmail.com>
2022-03-08 21:21:43 -0500

    added test cases

--- flatten.js
+++ flatten.js
@@ -20,4 +20,35 @@ const flatten = arr => {
   } return flattenedArr;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+/*------------------- TESTS -----------------*/
+
+/*
+* Function takes in two arrays and returns true or false based on a perfect match
+* @param {Array} arr1
+* @param {Array} arr2
+* @result {Boolean} true or false
+*/
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } return true;
+};
+
+// TEST FUNCTION FOR ARRAYS
+const assertArraysEqual = (arr1, arr2, expected) => {
+  if (eqArrays(arr1, arr2) === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// TEST CASES
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6], true);
+assertArraysEqual(flatten([[1, 2], [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6], true);
+assertArraysEqual(flatten(['a', ['b', 'c']]), ['a', 'b', 'c'], true);
