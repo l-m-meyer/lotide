b3ee3699359fd9c58ef6052d7abdc1f984f0d4f8
Lisa Meyer <lisamermey@gmail.com>
2022-03-09 18:12:37 -0500

    working solution and test cases

--- eqObjects.js
+++ eqObjects.js
@@ -5,6 +5,7 @@
 * @return {Boolean}
 */
 const eqObjects = (obj1, obj2) => {
+  // get object keys and store as an array
   const obj1Keys = Object.keys(obj1);
   const obj2Keys = Object.keys(obj2);
 
@@ -13,11 +14,50 @@ const eqObjects = (obj1, obj2) => {
     return false;
   }
 
+  // checks that other object does not include key found in first object
   for (const key of obj1Keys) {
     if (!obj2Keys.includes(key)) {
       return false;
     }
-  } return true;
+  }
+  
+  // check if key value is an array
+  for (const key of obj1Keys) {
+    if (Array.isArray(obj1[key]) && !Array.isArray(obj2[key])) {
+      return false;
+    } else if (!Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      return false;
+    } else if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      //both key values are arrays
+      // checks that the arrays are not equal
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
+    } else {
+      // checks that the keys do not match
+      if (obj1[key] !== obj2[key]){
+        return false;
+      }
+    }
+  } 
+  return true;
+};
+
+/*
+* Function takes in two arrays and returns true or false based on a perfect match
+* @param {Array} arr1
+* @param {Array} arr2
+* @result {Boolean} true or false
+*/
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } return true;
 };
 
 // FUNCTION IMPLEMENTATION
@@ -30,9 +70,18 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
+/** primatives as values */
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+assertEqual(eqObjects(ab, abc), false); // => false
+
+/** arrays as values */
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
