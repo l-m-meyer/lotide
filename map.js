031a361090e42e85b6bd4a983d89dab9cb1e26c9
Lisa Meyer <lisamermey@gmail.com>
2022-03-10 14:45:11 -0500

    testing the output of the for of loop

--- map.js
+++ map.js
@@ -1,10 +1,20 @@
 /*
 * Function will return a new array based on the results of the callback function.
 * @param {Array} arr
-* @param {Function} cb //shorthand for callback
-* @return {Array} newArr 
+* @param {Function} callback 
+* @return {Array} results 
 */
 const words = ["ground", "control", "to", "major", "tom"];
+
+const map = (arr, callback) => { 
+  const results = [];
+  for (const item of arr) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
+  return results;
+};
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
