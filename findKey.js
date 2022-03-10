6adf2d53d059c6fb5fe70d0cfb3845e069785f29
Lisa Meyer <lisamermey@gmail.com>
2022-03-10 15:52:11 -0500

    working solution

--- findKey.js
+++ findKey.js
@@ -8,8 +8,10 @@
  */
 const findKey = (obj, callback) => {
   for (const key in obj) {
-    console.log(callback(obj[key]));
-  }
+    if (callback(obj[key])) {
+      return key;
+    }
+  } return undefined;
 };
 
 findKey({
