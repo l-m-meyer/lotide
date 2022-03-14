c5fe96d2793ea5077ea1e4504316f096f67baa59
Lisa Meyer <lisamermey@gmail.com>
2022-03-14 15:37:39 -0400

    refactored middleTest.js to use Mocha syntax

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,15 +1,24 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-console.log('middle test');
-// TEST CASES
-let words = ["hello", "world", "lighthouse"];
-middle(words); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"], true);
+describe('#middle', () => {
+  it ("should return [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2], true);
-assertArraysEqual(middle([2, 3]), [], true);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3], true);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4], true);
-assertArraysEqual(middle([1, 2, 3, 4]), [2], false);
+  it ("should return [] for [2, 3]", () => {
+    assert.deepEqual(middle([2, 3]), []);
+  });
+  
+  it ("should return [2] for input [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it ("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it ("should return [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
