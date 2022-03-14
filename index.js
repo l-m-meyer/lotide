5640ad9d1e27c8f674346b6ae951b981bb59ebbf
Lisa Meyer <lisamermey@gmail.com>
2022-03-14 15:48:34 -0400

    refactored index.js to export head, middle, and tail functions at once

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-const assertEqual = require('./assertEqual');
-
-const eqArrays = require('./eqArrays');
-
 const head = require('./head');
 
 const middle = require('./middle');
 
 const tail = require('./tail');
+
+module.exports = {
+  head,
+  tail,
+  middle
+};
