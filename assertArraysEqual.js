a71134b3d1e0c74ab355c2e4b60e53921bd35ab2
Lisa Meyer <lisamermey@gmail.com>
2022-03-14 14:15:18 -0400

    refactored assertArraysEqual and created test file assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-/*
-* Function takes in two arrays and returns true or false based on a perfect match
-* @param {Array} arr1
-* @param {Array} arr2
-* @result {Boolean} true or false
-*/
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    } return true;
-};
+const eqArrays = require('./eqArrays');
 
 // TEST FUNCTION FOR ARRAYS
 const assertArraysEqual = (arr1, arr2, expected) => {
@@ -24,6 +9,4 @@ const assertArraysEqual = (arr1, arr2, expected) => {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3], true); // => should PASS
-assertArraysEqual([1, 2, 3], [1, 2, "3"], false); // => should PASS
-assertArraysEqual([1, 2, 3], [0, 12, -3, 8], false); // => should PASS
+module.exports = assertArraysEqual;
