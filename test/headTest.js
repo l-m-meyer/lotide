7825a7f58ae1d93695215e7160e2ef994605e111
Lisa Meyer <lisamermey@gmail.com>
2022-03-14 14:42:22 -0400

    refactored to use Mocha and Chai for testing, added describe block and it statements using Mocha syntax

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,16 @@
+const assert = require('chai').assert;
 const head = require('../head.js');
-const assertEqual = require('../assertEqual');
 
+describe('#head', () => {
+  it ('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),);
-assertEqual(head([1]), 1);
+  it ("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it ("returns nothing for []", () => {
+    assert.strictEqual(head([]),);
+  });
+});
