36737fd2c14b7b250d003427021a0754d5401ddb
Lisa Meyer <lisamermey@gmail.com>
2022-03-14 13:42:53 -0400

    refacted eqArrays.js to export module and created test file eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// TEST FUNCTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 /*
 * Function takes in two arrays and returns true or false based on a perfect match
 * @param {Array} arr1
@@ -24,6 +15,4 @@ const eqArrays = (arr1, arr2) => {
     } return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // => should PASS but fails
-assertEqual(eqArrays([1, 2, 3], [0, 12, -3, 8]), false); // => should PASS
+module.exports = eqArrays;
