5f6ece457115dd636fb808bafac8e64543a64861
Lisa Meyer <lisamermey@gmail.com>
2022-03-14 22:05:23 -0400

    included import and export for countOnly

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const countOnly = require('./countOnly');
 const flatten = require('./flatten');
 const head = require('./head');
 const middle = require('./middle');
 const tail = require('./tail');
 
 module.exports = {
+  countOnly,
   flatten,
   head,
   tail,
